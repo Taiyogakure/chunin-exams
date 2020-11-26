66a8f8c5c2abb5fe7151cde5326d45931539a3d3
Ambareen09 <ambubushra@gmail.com>
2020-11-26 19:03:32 +0530

    created sign up page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./routes/user"); //new addition
+const InitiateMongoServer = require("./config/db");
+// Initiate Mongo Server
+InitiateMongoServer();
+const app = express();
+// PORT
+const PORT = process.env.PORT || 4000;
+// Middleware
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
 });
-
-module.exports = router;
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
