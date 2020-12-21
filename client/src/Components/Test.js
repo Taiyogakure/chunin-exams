1003ce5c82118e2cecb2cd0226313131ea07de01
AzharMallick <63769407+AzharMallick@users.noreply.github.com>
2020-12-21 21:02:28 +0530

    li to <a>

--- client/src/Components/Test.js
+++ client/src/Components/Test.js
@@ -16,9 +16,9 @@ export default function Test() {
             <div id="quest">
                 <div id="lefty">
                     <ol>
-                        <li>
+                        <a>
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                        </a>
                         <li>
                             It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
                         </li>
