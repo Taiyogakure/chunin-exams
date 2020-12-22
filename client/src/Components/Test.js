5f51824b1437ead8de055caa37d7e09dc2d5a364
AzharMallick <63769407+AzharMallick@users.noreply.github.com>
2020-12-22 20:55:52 +0530

    added some buttons and options

--- client/src/Components/Test.js
+++ client/src/Components/Test.js
@@ -5,53 +5,60 @@ import './Test.scss';
 export default function Test() {
     return(
         <body id="dash">
-            <nav id="header">
-                <div id="header-nav">
-                    <div id="logo">
-                        Chunin Exam
-                    </div>
-                    <button id="end">End Test</button>
-                </div>
-            </nav>
+            <header>
+                <a href="#" className="logo">Chunin Exam</a>
+                <ul>
+                    <li id="timer">Time Left</li>
+                    <li><button type="submit" id="end"><a href="http://localhost:3000/Components/Feedback">End Test</a></button></li>
+                </ul>
+            </header>
             <div id="quest">
                 <div id="lefty">
                     <ol>
-                        <a>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </a>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a> 
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                         <li>
-                            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
-                        </li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
+                        <li>
+                            <a href="">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</a>
+                        </li><hr></hr>
                     </ol>
                 </div>
                 <div id="righty">
                     <p>
                         Full Question
                     </p>
+                    <section id="options">
+                        <ul>
+                            <li><button type="radio" className="opt">option 1</button></li>
+                            <li><button type="radio" className="opt">option 2</button></li>
+                            <li><button type="radio" className="opt">option 3</button></li>
+                            <li><button type="radio" className="opt">option 4</button></li>
+                        </ul>
+                    </section>
                 </div>
             </div>
             <footer id="ver">Version 1.0</footer>
