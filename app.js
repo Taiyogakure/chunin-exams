f9c568b9829d857dcc7adb20d9f60b48b6c64f89
Azan <azanulhaque@gmail.com>
2020-12-22 23:11:13 +0530

    Debugging

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user');
+// var usersRouter = require('./routes/user');
 
 var app = express();
 
@@ -15,12 +17,14 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser);
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 // Middleware
 app.use(bodyParser.json());
+// This application level middleware prints incoming requests to the servers console, useful to see incoming requests
+app.use((req, res, next) => {
+  console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+  next();
+});
 app.get("/", (req, res) => {
   res.json({message: "API Working!"});
   // res.sendFile(path.join(__dirname, 'client/src/index.js'));
