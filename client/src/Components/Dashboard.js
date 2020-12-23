6e51af4ede68a005a583e7531e62abd8a3bba5ad
AzharMallick <63769407+AzharMallick@users.noreply.github.com>
2020-12-23 10:43:46 +0530

    added header and footer

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -3,6 +3,18 @@ import './Dashboard.scss';
 
 export default function Dashboard() {
     return(
-        <body id="dash">hello!!!</body>
+        <body id="dash">
+            <header>
+                <a href="#" className="logo">Chunin Exam</a>
+                <ul>
+                    <li><button type="submit" id="logout"><a href="#">Logout</a></button></li>
+                </ul>
+            </header>
+            <section>
+                waon chika waon waon!!!!
+
+            </section>
+            <footer id="ver">Version 1.0</footer>
+        </body>
     )
 }
