d22b70632d9ca7680a158753c34879050fbf004c
AzharMallick <63769407+AzharMallick@users.noreply.github.com>
2020-12-25 17:25:57 +0530

    dasboard styling

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -10,12 +10,14 @@ export default function Dashboard() {
                     <li><button type="submit" id="logout"><a href="#">Logout</a></button></li>
                 </ul>
             </header>
-            <div className="side-nav">
-                <a href="#leaderboard">LeaderBoard</a>
-                <a href="#Q">Questions</a>
-            </div>
             <section className="conts">
-
+                <div className="side-nav">
+                    <a href="#leaderboard">LeaderBoard</a>
+                    <a href="#Q">Questions</a>
+                </div>
+                <div className="table">
+                    <img src="https://images.unsplash.com/photo-1608869984089-85a581648a72?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="lead-pic"/>
+                </div>
             </section>
             <footer id="ver">Version 1.0</footer>
         </body>
