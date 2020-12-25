872362536ba5e2dff90bf3c3337ce987b99200a8
AzharMallick <63769407+AzharMallick@users.noreply.github.com>
2020-12-25 21:25:41 +0530

    admin board redesigned

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -4,19 +4,34 @@ import './Dashboard.scss';
 export default function Dashboard() {
     return(
         <body id="dash">
-            <header>
-                <a href="#" className="logo">Chunin Exam</a>
-                <ul>
-                    <li><button type="submit" id="logout"><a href="#">Logout</a></button></li>
-                </ul>
-            </header>
             <section className="conts">
                 <div className="side-nav">
+                    <h2><span>C</span>hunin <span>E</span>xam</h2>
                     <a href="#leaderboard">LeaderBoard</a>
                     <a href="#Q">Questions</a>
+                    <button type="submit">Sign out</button>
                 </div>
                 <div className="table">
-                    <img src="https://images.unsplash.com/photo-1608869984089-85a581648a72?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOXx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" className="lead-pic"/>
+                    <a name="leaderboard">
+                        <img src="https://cdn.dribbble.com/users/1169554/screenshots/9404247/media/edd785e9eb34b2b13a4056d91102ac06.png?compress=1&resize=1000x750" className="lead-pic"/>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Tortor at risus viverra adipiscing at in tellus. Sed risus pretium quam vulputate dignissim. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Nec ultrices dui sapien eget mi proin sed libero. Tincidunt dui ut ornare lectus sit amet. Integer quis auctor elit sed. Quam vulputate dignissim suspendisse in est ante in nibh. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Nisi porta lorem mollis aliquam ut porttitor. Dui sapien eget mi proin sed libero enim. Lacus suspendisse faucibus interdum posuere lorem ipsum. Ut placerat orci nulla pellentesque dignissim enim sit.
+
+                        Mi quis hendrerit dolor magna eget. Consequat id porta nibh venenatis cras. Bibendum at varius vel pharetra vel. Egestas diam in arcu cursus euismod. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Justo laoreet sit amet cursus sit. In eu mi bibendum neque egestas congue quisque egestas. Turpis egestas integer eget aliquet nibh. Duis ultricies lacus sed turpis tincidunt. Lorem mollis aliquam ut porttitor. Etiam tempor orci eu lobortis elementum nibh tellus. Congue mauris rhoncus aenean vel elit. Nullam non nisi est sit amet facilisis magna etiam tempor. Urna cursus eget nunc scelerisque viverra mauris. Tristique senectus et netus et malesuada fames ac. Purus ut faucibus pulvinar elementum.
+
+                        Mi quis hendrerit dolor magna eget. Consequat id porta nibh venenatis cras. Bibendum at varius vel pharetra vel. Egestas diam in arcu cursus euismod. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Justo laoreet sit amet cursus sit. In eu mi bibendum neque egestas congue quisque egestas. Turpis egestas integer eget aliquet nibh. Duis ultricies lacus sed turpis tincidunt. Lorem mollis aliquam ut porttitor. Etiam tempor orci eu lobortis elementum nibh tellus. Congue mauris rhoncus aenean vel elit. Nullam non nisi est sit amet facilisis magna etiam tempor. Urna cursus eget nunc scelerisque viverra mauris. Tristique senectus et netus et malesuada fames ac. Purus ut faucibus pulvinar elementum.
+                        </p>
+                    </a>
+                    <a name="Q">
+                        <img src="https://cdn.dribbble.com/users/257709/screenshots/3779497/dashboard_d_m_shot_50_.png?compress=1&resize=800x600" className="lead-pic"></img>
+                        <p>
+                        Mi quis hendrerit dolor magna eget. Consequat id porta nibh venenatis cras. Bibendum at varius vel pharetra vel. Egestas diam in arcu cursus euismod. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Justo laoreet sit amet cursus sit. In eu mi bibendum neque egestas congue quisque egestas. Turpis egestas integer eget aliquet nibh. Duis ultricies lacus sed turpis tincidunt. Lorem mollis aliquam ut porttitor. Etiam tempor orci eu lobortis elementum nibh tellus. Congue mauris rhoncus aenean vel elit. Nullam non nisi est sit amet facilisis magna etiam tempor. Urna cursus eget nunc scelerisque viverra mauris. Tristique senectus et netus et malesuada fames ac. Purus ut faucibus pulvinar elementum.
+
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel orci porta non pulvinar neque laoreet suspendisse interdum consectetur. Tortor at risus viverra adipiscing at in tellus. Sed risus pretium quam vulputate dignissim. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Nec ultrices dui sapien eget mi proin sed libero. Tincidunt dui ut ornare lectus sit amet. Integer quis auctor elit sed. Quam vulputate dignissim suspendisse in est ante in nibh. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Nisi porta lorem mollis aliquam ut porttitor. Dui sapien eget mi proin sed libero enim. Lacus suspendisse faucibus interdum posuere lorem ipsum. Ut placerat orci nulla pellentesque dignissim enim sit.
+
+                        Mi quis hendrerit dolor magna eget. Consequat id porta nibh venenatis cras. Bibendum at varius vel pharetra vel. Egestas diam in arcu cursus euismod. Pellentesque pulvinar pellentesque habitant morbi tristique senectus. Justo laoreet sit amet cursus sit. In eu mi bibendum neque egestas congue quisque egestas. Turpis egestas integer eget aliquet nibh. Duis ultricies lacus sed turpis tincidunt. Lorem mollis aliquam ut porttitor. Etiam tempor orci eu lobortis elementum nibh tellus. Congue mauris rhoncus aenean vel elit. Nullam non nisi est sit amet facilisis magna etiam tempor. Urna cursus eget nunc scelerisque viverra mauris. Tristique senectus et netus et malesuada fames ac. Purus ut faucibus pulvinar elementum.
+                        </p>
+                    </a>
                 </div>
             </section>
             <footer id="ver">Version 1.0</footer>
